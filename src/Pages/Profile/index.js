7cde1ab7007727e80205fc5947269ef33b7f1fe6
meeran <subhan@gmail.com>
2022-05-21 17:14:16 +0500

    Heelo

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,7 +1,94 @@
 import React from "react";
-
+import "./profile.css";
 function index() {
-  return <div>index</div>;
+  return (
+    <section>
+      <div class="rt-container">
+        <div class="col-rt-12">
+          <div class="Scriptcontent">
+            <div class="student-profile py-4">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-4">
+                    <div class="card shadow-sm">
+                      <div class="card-header bg-transparent text-center">
+                        <img
+                          class="profile_img"
+                          src="https://source.unsplash.com/600x300/?student"
+                          alt="student dp"
+                        />
+                        <h3>Ishmam Ahasan Samin</h3>
+                      </div>
+                      <div class="card-body">
+                        <p class="mb-0">
+                          <strong class="pr-1">User ID:</strong>321000001
+                        </p>
+                        <p class="mb-0">
+                          <strong class="pr-1">Profession:</strong>4
+                        </p>
+                        <p class="mb-0">
+                          <strong class="pr-1">Address:</strong>A
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-8">
+                    <div class="card shadow-sm">
+                      <div class="card-header bg-transparent border-0">
+                        <h3 class="mb-0">
+                          <i class="far fa-clone pr-1"></i>General Information
+                        </h3>
+                      </div>
+                      <div class="card-body pt-0">
+                        <table class="table table-bordered">
+                          <tr>
+                            <th width="30%">Email</th>
+                            <td width="2%">:</td>
+                            <td>meeranfaraz@hothold.com</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Date of Birth</th>
+                            <td width="2%">:</td>
+                            <td>01/02/2020</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Gender</th>
+                            <td width="2%">:</td>
+                            <td>Male</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Star Count</th>
+                            <td width="2%">:</td>
+                            <td>4999</td>
+                          </tr>
+                          <tr>
+                            <th width="30%">Hobbies</th>
+                            <td width="2%">:</td>
+                            <td>knitting, poetry, poondi</td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                    <div style={{ height: "26px" }}></div>
+                    <div class="card shadow-sm">
+                      <div class="card-header bg-transparent border-0">
+                        <h3 class="mb-0">
+                          <i class="far fa-clone pr-1"></i>Created Organizations
+                        </h3>
+                      </div>
+                      <div class="card-body pt-0">
+                        <ul></ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default index;
